aa88a6b74c293aa2505f6ceb3da8239cd5794d62
dragondouble_lin <1007193218@qq.com>
2021-08-05 12:32:42 +0800

    第一次测试修改。

--- Hello.java
+++ Hello.java
@@ -2,4 +2,13 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello github!");
     }
+class dog{
+        int gae;
+        public void eat(){
+            System.out.println("狗狗吃饭！");
+        }
+}
+    /*
+    第一次修改测试
+     */
 }
